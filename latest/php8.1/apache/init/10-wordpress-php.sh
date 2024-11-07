1497eb386670c4288d6629c25ed0df15c97002d0
Pascal GAULT <pascal@inrage.fr>
2024-11-07 01:03:54 +0100

    feat: add production mu-plugins and create upgrade director

--- latest/php8.1/apache/init/10-wordpress-php.sh
+++ latest/php8.1/apache/init/10-wordpress-php.sh
@@ -3,7 +3,7 @@
 set -e
 
 start_redis() {
-  if wp redis &> /dev/null; then
+  if wp redis &>/dev/null; then
     echo "Redis is installed... enabling Redis object cache..."
     wp redis enable --force
   else
@@ -11,14 +11,23 @@ start_redis() {
   fi
 }
 
+CONTENT_DIR=$(wp eval 'echo WP_CONTENT_DIR;')
+
 _gotpl() {
-    if [[ -f "/etc/gotpl/$1" ]]; then
-        gotpl "/etc/gotpl/$1" > "$2"
-    fi
+  if [[ -f "/etc/gotpl/$1" ]]; then
+    gotpl "/etc/gotpl/$1" >"$2"
+  fi
 }
 
 if [[ "${WORDPRESS_NO_CREATE_CONFIG}" != "true" ]]; then
-    _gotpl "wp-config.php.tmpl" "/var/www/html/wp-config.php"
+  _gotpl "wp-config.php.tmpl" "/var/www/html/wp-config.php"
+fi
+
+_gotpl "production.php.tmpl" "${CONTENT_DIR}/mu-plugins/production.php"
+
+## Create upgrade directory
+if [[ ! -d "${CONTENT_DIR}/upgrade" ]]; then
+  mkdir -p "${CONTENT_DIR}/upgrade"
 fi
 
 start_redis
